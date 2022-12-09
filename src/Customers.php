65bf13079d505961332cfbf264b774ec87a02acd
Firza Aditiya <firzaaditiya22@gmail.com>
2022-12-09 14:13:35 +0700

    Example Simple Library

---
+++ src/Customers.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Comgit;
+
+class Customers
+{
+    public function __construct(private string $name){}
+
+    public function sayHello(string $name) : string
+    {
+        return ("Hello $name, My name is $this->name");
+    }
+}
